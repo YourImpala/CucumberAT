76b7b977b84dc625b40d61065f75f6f5c7834317
YourImpala <dmitriy.petrov.1993@mail.ru>
2020-10-13 08:14:49 +0300

    refactor scenario to ru

--- src/test/java/sberbank/cucumber/steps/CucumberSteps.java
+++ src/test/java/sberbank/cucumber/steps/CucumberSteps.java
@@ -2,6 +2,9 @@ package sberbank.cucumber.steps;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+import cucumber.api.java.ru.И;
+import cucumber.api.java.ru.Когда;
+import cucumber.api.java.ru.Тогда;
 import io.cucumber.datatable.DataTable;
 import sberbank.cucumber.GeneralMethods;
 
@@ -13,7 +16,7 @@ public class CucumberSteps {
     ChoosePolicyTravelInsuranceSteps choosePolicyTravelInsuranceSteps;
     FormTravelInsuranceSteps formTravelInsuranceSteps;
 
-    @Given("Перейти на сайт '(.*)'")
+    @Когда("перешел на сайт '(.*)'")
     public void перейти_на_сайт(String url) {
         Hooks.getDriver().get(url);
 
@@ -25,67 +28,67 @@ public class CucumberSteps {
 
     }
 
-    @Then("Нажать на пункт навигации '(.*)'")
+    @Тогда("нажать на пункт навигации '(.*)'")
     public void нажать_на_пункт_навигации(String navItemName) {
         privateClientSteps.selectInNavigationMenu(navItemName);
     }
 
-    @Then("Нажать на пункт выпадающего меню '(.*)'")
+    @Тогда("нажать на пункт выпадающего меню '(.*)'")
     public void нажать_на_пункт_выпадающего_меню(String dropdownItemName) {
         privateClientSteps.selectInDropdownMenu(dropdownItemName);
     }
 
-    @Then("Нажать на кнопку 'оформить онлайн' в блоке страховки")
+    @Когда("нажал на кнопку 'оформить онлайн' в блоке страховки")
     public void нажать_на_оформить_онлайн() {
         insureMainSteps.applyOnlineInsurance();
     }
 
-    @Then("Проверить открытие вкладки '(.*)'")
+    @Тогда("проверить открытие вкладки '(.*)'")
     public void проверить_открытие_вкладки(String pageTitleName) {
         insureMainSteps.checkNewTab(pageTitleName);
     }
 
-    @Then("Проверить наличие на странице заголовка '(.*)'")
+    @Когда("на странице присутствует заголовок '(.*)'")
     public void проверить_наличие_на_странице_заголовка(String titleName) {
         travelIsuranceSteps.checkTitle(titleName);
     }
 
-    @Then("Нажать на кнопку Оформить Онлайн")
+    @Тогда("нажать на кнопку Оформить Онлайн")
     public void нажать_на_кнопку_оформить_онлайн() throws InterruptedException {
         travelIsuranceSteps.clickIssueOnlineButton();
     }
 
-    @Then("Выбрать полис - '(.*)'")
+    @Когда("выбран пакет страховки - '(.*)'")
     public void выбрать_полис(String policyType) {
         choosePolicyTravelInsuranceSteps.selectInsuranceType(policyType);
     }
 
-    @Then("Проверить выбор полиса - '(.*)'")
+    @Тогда("проверить выбор пакета страховки - '(.*)'")
     public void провенить_выбор_полиса(String policyType) {
         choosePolicyTravelInsuranceSteps.checkSelectedInsurance(policyType);
     }
 
-    @Then("Нажать кнопку Оформить")
+    @Тогда("нажать кнопку Оформить")
     public void нажать_кнопку_оформить() {
         choosePolicyTravelInsuranceSteps.clickArrangeButton();
     }
 
-    @Then("Заполнить поля формы")
+    @Когда("заполнены поля формы")
     public void заполнить_форму(DataTable fieldsAndValues) {
         formTravelInsuranceSteps.fillFields(GeneralMethods.getMapFromTable(fieldsAndValues));
     }
 
-    @Then("Проверить заполнение полей")
+    @Тогда("Проверить заполнение полей")
     public void роверить_заполние_полей(DataTable fieldsAndValues) {
         formTravelInsuranceSteps.checkFieldsFilling(GeneralMethods.getMapFromTable(fieldsAndValues));
     }
 
-    @Then("Нажать кнопку продолжить")
+    @Тогда("нажать кнопку продолжить")
     public void нажать_кнопку_продолжить() {
         formTravelInsuranceSteps.clickContinueButton();
     }
 
-    @Then("Проверить сообщение об ошибке - '(.*)'")
+    @И("проверить сообщение об ошибке - '(.*)'")
     public void проверить_сообщение_об_ошибке(String errorText) {
         formTravelInsuranceSteps.checkErrorMessage(errorText);
     }
